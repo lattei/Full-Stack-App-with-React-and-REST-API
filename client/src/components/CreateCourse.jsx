11f1c6fdac01db95b39e0590725f20766d056d45
Carline Senat <carlinesenat@gmail.com>
2024-12-15 20:57:18 -0800

    Complete Create Course
    Complete create course component, not yet tested, but it shows via npm run dev to vite client

--- client/src/components/CreateCourse.jsx
+++ client/src/components/CreateCourse.jsx
@@ -11,40 +11,35 @@ const CreateCourse = () => {
 
     // Setting up state similar to how I've set up signup
     
-    // const firstName = useRef(null);
-    // const lastName = useRef(null);
-    // const emailAddress = useRef(null);
-    // const password = useRef(null);
-    // const [errors, setErrors] = useState([]);
-
-        
-
-    //     const user = {
-    //         firstName: firstName.current.value,
-    //         lastName: lastName.current.value,
-    //         emailAddress: emailAddress.current.value,
-    //         password: password.current.value
-    //     };
-
-    /* course props:
-        title
-        description
-        estimated time
-        materialsneeded
-        */
-
+    const title = useRef(null);
+    const description = useRef(null);
+    const estimatedTime = useRef(null);
+    const materialsNeeded = useRef(null);
+    const [errors, setErrors] = useState([]);
 
 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        const course = {
+            userId: authUser.id,
+            title: title.current.value,
+            description: description.current.value,
+            estimatedTime: estimatedTime.current.value,
+            materialsNeeded: materialsNeeded.current.value
+        };
+
+        /* In the handleSubmit function, send a POST request to the REST API's /api/courses endpoint with the user id, course title, description, estimated time, and materials needed in the body of the request */
+
         try {
-            const response = await api("/users", "POST", user);
+            const response = await api("/courses", "POST", course, authUser);
+
+            /* After successfully creating the new course, redirect the user to the "Courses" or "Course Detail" screen for the newly created course. */
             if (response.status === 201) {
-                console.log(`Successfully signed up and authenticated.`);
-                await actions.signIn(user.emailAddress, user.password);
-                navigate("/courses");
+                console.log(`Course has been created.`);
+                const courseId = response.data;
+                navigate(`/courses/${courseId}`);
 
             } else if (response.status === 400) {
                 const data = await response.json();
@@ -72,6 +67,7 @@ const CreateCourse = () => {
             <h2>Create Course</h2>
             <div className="validation--errors">
                 <h3>Validation Errors</h3>
+                {/* change this to error component */}
                 <ul>
                     <li>Please provide a value for "Title"</li>
                     <li>Please provide a value for "Description"</li>
@@ -85,29 +81,32 @@ const CreateCourse = () => {
                         id="courseTitle" 
                         name="courseTitle" 
                         type="text" 
-                        ref={course.title}
+                        ref={title}
                         />
 
-                            <p>By {authUser.firstName} {authUser.lastName}</p>
+                            {/* <p>By {authUser.firstName} {authUser.lastName}</p> */}
 
                             <label htmlFor="courseDescription">Course Description</label>
                             <textarea 
                             id="courseDescription" 
                             name="courseDescription"
-                            value
+                            ref={description}
                             >
 
                             </textarea>
                     </div>
                     <div>
-                        <label for="estimatedTime">Estimated Time</label>
-                        <input id="estimatedTime" name="estimatedTime" type="text" value="">
+                        <label htmlFor="estimatedTime">Estimated Time</label>
+                        <input id="estimatedTime" name="estimatedTime" 
+                        type="text" 
+                        ref={estimatedTime} />
 
-                            <label for="materialsNeeded">Materials Needed</label>
-                            <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
+                            <label htmlFor="materialsNeeded">Materials Needed</label>
+                            <textarea id="materialsNeeded" name="materialsNeeded"
+                            ref={materialsNeeded}></textarea>
                     </div>
                 </div>
-                <button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                <button className="button" type="submit">Create Course</button><button className="button button-secondary" onClick={handleCancel}>Cancel</button>
             </form>
         </div>
     );
